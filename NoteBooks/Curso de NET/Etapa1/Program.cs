9512bbacb413a0d6d2cf28c2b2775286df154ca8
Alejandro-sin <alejandro.chem.io@gmail.com>
2021-03-11 08:34:19 -0500

    update charp

---
+++ NoteBooks/Curso de NET/Etapa1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //invoca al constructor.
+            var escuela = new Escuela("Platzi .NET Dojo", 2021);
+            escuela.Pais = "Colombia";
+            escuela.Ciudad = "Bogota";
+
+            //asigno nombre
+
+            Console.WriteLine(escuela.Nombre);
+            Console.WriteLine(escuela.AñoDeCreación);
+        }
+    }
+}
